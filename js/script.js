5608f852450da7b2b9abed6f217e2ff3135eb474
Gabriel Moreira Araújo <moreira.araujo99@gmail.com>
2023-07-11 15:50:54 -0300

    fix: arrumado layout no mobile e no desktop

--- js/script.js
+++ js/script.js
@@ -3,20 +3,19 @@ const formInput = document.querySelector("#form-input");
 const todoList = document.querySelector("#todo-list");
 var btnAdd = document.querySelector("#btn-add");
 
-function createTodo(){
-    
+function createTodo() {
     // PEGANDO O QUE FOI DIGITADO NO INPUT
     const inputValue = formInput.value;
- 
+
     // NÃO CRIAR NENHUMA TAREFA, CASO CAMPO FOR VAZIO.
-    if(inputValue == "" || inputValue == null ){
-        
-    } else{
+    if (inputValue == "" || inputValue == null) {
+        alert("Preencha o campo!");
+    } else {
         // CRIANDO OS ELEMENTOS
         var todo = document.createElement("div");
         todo.classList.add("todo");
         todoList.appendChild(todo);
-      
+
         const todoInput = document.createElement("input");
         todoInput.setAttribute("value", inputValue);
         todoInput.setAttribute("type", "text");
@@ -26,79 +25,68 @@ function createTodo(){
         const todoBtns = document.createElement("div");
         todoBtns.classList.add("todo-btns");
         todo.appendChild(todoBtns);
-      
+
         const doneBtn = document.createElement("button");
         doneBtn.classList.add("finish-todo");
+        doneBtn.title = "Concluir tarefa";
         doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
         todoBtns.appendChild(doneBtn);
-      
+
         const editBtn = document.createElement("button");
         editBtn.classList.add("edit-todo");
+        editBtn.title = "Editar tarefa";
         editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
         todoBtns.appendChild(editBtn);
-      
+
         const deleteBtn = document.createElement("button");
         deleteBtn.classList.add("delete-todo");
+        deleteBtn.title = "Apagar tarefa";
         deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
         todoBtns.appendChild(deleteBtn);
 
         // APAGAR INPUT APÓS CRIAR TAREFA
-        formInput.value = ""
+        formInput.value = "";
         // FOCAR NO INPUT APÓS CRIAR TAREFA
         formInput.focus();
 
         //FUNÇÕES PARA OS BOTÕES
-        doneBtn.addEventListener("click", finish)
-        
-        function finish(){
+        doneBtn.addEventListener("click", finish);
 
-            todo.classList.toggle("done")
-            editBtn.classList.toggle("hide")         
-
+        function finish() {
+            todo.classList.toggle("done");
+            editBtn.classList.toggle("hide");
         }
 
-        editBtn.addEventListener("click", edit)
+        editBtn.addEventListener("click", edit);
 
-        function edit(){
-
-            if(todoInput.disabled === true){
-
-                editBtn.innerHTML = '<i class="fa-solid fa-check-double"></i>'
+        function edit() {
+            if (todoInput.disabled === true) {
+                editBtn.innerHTML = '<i class="fa-solid fa-check-double"></i>';
                 doneBtn.classList.add("hide");
                 todoInput.disabled = false;
                 todoInput.focus();
-
             } else {
-
                 todoInput.disabled = true;
                 doneBtn.classList.remove("hide");
-                editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+                editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
             }
-            
-
         }
 
-        deleteBtn.addEventListener("click", delet)
+        deleteBtn.addEventListener("click", delet);
 
-        function delet(){
-
-            let removeTodo = confirm("Tem certeza?")
-            if(removeTodo == true){
+        function delet() {
+            let removeTodo = confirm("Tem certeza?");
+            if (removeTodo == true) {
                 todo.remove();
             }
-
-            
-        }         
-    }   
-        
+        }
+    }
 }
 
 // ATIVANDO A TECLA ENTER
 
 document.addEventListener("keypress", (e) => {
-            
-    if (e.code === "Enter" || e.code === "NumpadEnter"){
-
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
         btnAdd.click();
     }
-})
+});
